929cde188260d5b1226941221d8ca273212f46ad
Jonathan-Te <jonathantbtwd@gmail.com>
2024-01-25 22:39:26 -0500

    creacion proyecto angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,CardModule,ButtonModule,TableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
+  horarios: any=[
+    {
+      numero: 1,
+      dia: "Lunes",
+      horaInicio: "06:00",
+      horaFin: "09:00",
+      placaRes: "1 y 2"
+    }
+  ];
+
 }
